23ead999abf277726ef754cb3d7c27b445fb2acf
David Balsas Martín <dasdadbm@gmail.com>
2022-07-22 16:02:48 +0200

    Update LongestPassword.java for performance
    Deleted String.chars().allMatch(...) inside the for loop.
    Added Pattern.compile() before the loop and Pattern.matcher(word).find() inside the for loop.

--- codility/exercises/LongestPassword.java
+++ codility/exercises/LongestPassword.java
@@ -6,6 +6,7 @@ package com.codility.app.programmers.trainings.longest-password;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.regex.Pattern;
 
 class Solution {
     public int solution(String S) {
@@ -15,10 +16,12 @@ class Solution {
         Arrays.sort(words, Comparator.comparingInt(String::length).reversed());
         // If there is no match, we have to return -1
         int response = -1;
+        // Compile the pattern before the loop
+        Pattern p = Pattern.compile("^[a-zA-Z0-9]*$");
         for (String word : words) {
             int wordLength = word.length();
             // Check that the word length is odd and it has only alphanumerical characters
-            if (wordLength % 2 != 0 && word.chars().allMatch(Character::isLetterOrDigit)) {
+            if (wordLength % 2 != 0 && p.matcher(word).find()) {
                 String letters = word.replaceAll("[^aA-zZ]", "");
                 // If the length of the letters are even, this is the longest password
                 if (letters.length() % 2 == 0) {
