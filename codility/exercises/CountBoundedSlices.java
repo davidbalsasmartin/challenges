7cf32005e01c1455aa2927f7e0796dc9f1a93bbf
David Balsas Martín <dasdadbm@gmail.com>
2022-07-26 00:11:26 +0200

    Update CountBoundedSlices.java
    Update searching the maximum and minimum number by hand (common for loop) for a better performance.
    Still searching a better way to do it.

--- codility/exercises/CountBoundedSlices.java
+++ codility/exercises/CountBoundedSlices.java
@@ -9,15 +9,33 @@ import java.util.*;
 class Solution {
     public int solution(int K, int[] A) {
         int counter = 0;
+        int max = 1000000000;
         for (int i = 0; i < A.length; i++) {
-            List<Integer> numbers = new ArrayList<>();
-            for (int x = 0; x < A.length - i; x++) {
-                numbers.add(A[i + x]);
-                if (numbers.size() == 1 ||(Collections.max(numbers) - Collections.min(numbers)) <= K) {
+            for (int x = i; x < A.length; x++) {
+                if (difference(A, i, x) <= K) {
                     counter++;
+                    if (counter == max) {
+                        return max;
+                    }
+                } else {
+                    break;
                 }
             }
         }
         return counter;
+    }
+
+    private int difference(int[] A, int first, int last) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = first; i <= last; i++) {
+            if (A[i] < min) {
+                min = A[i];
+            }
+            if (A[i] > max) {
+                max = A[i];
+            }
+        }
+        return max - min;
     }
 }
