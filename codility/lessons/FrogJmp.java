8bf5d33fade5d6c217b3d2115a6d5900ab2fdcd3
David Balsas Martín <dasdadbm@gmail.com>
2022-07-27 14:19:43 +0200

    Create FrogJmp.java

---
+++ codility/lessons/FrogJmp.java
@@ -0,0 +1,15 @@
+package com.codility.app.programmers.trainings.lessons.3-time-complexity.frog-jmp;
+
+/**
+*	https://app.codility.com/ exercise solved by @author davidbalsasmartin
+**/
+
+class Solution {
+    public int solution(int X, int Y, int D) {
+        if (X == Y) {
+            return 0;
+        }
+        // Calculate how many jumps left to reach the objetive, rounding to the highest number
+        return (int) Math.ceil((double)(Y - X) / D);
+    }
+}
