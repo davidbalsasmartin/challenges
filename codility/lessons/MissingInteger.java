55bbf60bf053640c1cd0e51f687f50c78609d71f
David Balsas Martín <dasdadbm@gmail.com>
2022-07-30 15:45:25 +0200

    Create MissingInteger.java

---
+++ codility/lessons/MissingInteger.java
@@ -0,0 +1,21 @@
+/**
+*	https://app.codility.com/ exercise solved by @author davidbalsasmartin
+**/
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A) {
+        int result = 1;
+        Arrays.sort(A);
+        for (int i : A) {
+            if (i == result) {
+                result++;
+            } else if (i > result) {
+                break;
+            }
+        }
+        return result;
+    }
+}
