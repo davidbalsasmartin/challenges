4a422af3e1ef9bd5f7dc77780e65384ca1714c39
David Balsas Martín <dasdadbm@gmail.com>
2022-08-01 23:19:40 +0200

    Changed comment in PermCheck.java

--- codility/lessons/PermCheck.java
+++ codility/lessons/PermCheck.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] A) {
         Arrays.sort(A);
-        // The result will be 1, and if there is not any number correct, then it will be changed to 0
+        // The result will be 1, and if any number is missing, then it will be changed to 0
         int result = 1;
         for (int i = 0; i < A.length; i++) {
             if ((i + 1) != A[i]) {
