11ee8202782aa373fd72600abc73489875c64e0c
David Balsas Martín <dasdadbm@gmail.com>
2022-08-01 23:18:03 +0200

    Create PermCheck.java

---
+++ codility/lessons/PermCheck.java
@@ -0,0 +1,22 @@
+package com.codility.app.programmers.lessons.counting-elements.perm-check;
+
+/**
+*	https://app.codility.com/ exercise solved by @author davidbalsasmartin
+**/
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        // The result will be 1, and if there is not any number correct, then it will be changed to 0
+        int result = 1;
+        for (int i = 0; i < A.length; i++) {
+            if ((i + 1) != A[i]) {
+                --result;
+                break;
+            }
+        }
+        return result;
+    }
+}
