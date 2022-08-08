87740ebc88d460308739fa1daee3d596324d8d98
David Balsas Martín <dasdadbm@gmail.com>
2022-08-08 12:03:59 +0200

    Create Fish.java

---
+++ codility/lessons/Fish.java
@@ -0,0 +1,31 @@
+package com.codility.app.programmers.lessons.stacks-and-queues.fish;
+
+/**
+*	https://app.codility.com/ exercise solved by @author davidbalsasmartin
+**/
+
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int counterUp = 0;
+        Stack<Integer> downstream = new Stack<>();
+        for (int i = 0; i < A.length; i++) {
+            if (B[i] == 0) {
+                if (downstream.size() == 0) {
+                    counterUp++;
+                } else {
+                    while (downstream.size() > 0 && downstream.peek() < A[i]) { // Delete all the fishes downstream if the fish that goes upstream is bigger
+                        downstream.pop(); 
+                    }
+                    if (downstream.size() == 0) { // If all the the fishes downstream are eaten, the fish upstream will survive
+                        counterUp++;
+                    }
+                }
+            } else {
+                downstream.addElement(A[i]);
+            }
+        }
+        return counterUp + downstream.size(); // Add the downstreams and upstreams fishes alives
+    }
+}
