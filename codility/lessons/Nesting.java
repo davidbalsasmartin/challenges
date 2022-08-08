5338b1deb3f0e2ca4b60a787625e4b7ee3aa17b6
David Balsas Martín <dasdadbm@gmail.com>
2022-08-08 13:59:25 +0200

    Create Nesting.java

---
+++ codility/lessons/Nesting.java
@@ -0,0 +1,28 @@
+package com.codility.app.programmers.trainings.stacks-and-queues.nesting;
+
+/**
+*	https://app.codility.com/ exercise solved by @author davidbalsasmartin
+*
+*   This exercise is correct but it is a bit slow, I am still searching a better performance
+**/
+
+import java.util.Stack;
+
+class Solution {
+    public int solution(String S) {
+        if (S.length() == 0) {
+            return 1;
+        }
+        int bracketsNotClosed = 0;
+        for (char i : S.toCharArray()) {
+            if (i == '(') { // If it is an open special character, save it
+                bracketsNotClosed++;
+            } else if (bracketsNotClosed == 0) { // If it is an closing character and there are not bracketsNotClosed saved before, it is not well nested
+                return 0;
+            } else { // If it is an closing character and there are bracketsNotClosed saved before, delete one
+                bracketsNotClosed--;
+            }
+        }
+        return bracketsNotClosed > 0 ? 0 : 1; // If the char array is checked and there are bracketsNotClosed, it is not well nested
+    }
+}
