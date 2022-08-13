b5e507ca6bfd466505601555e99f558954bce564
David Balsas Martín <dasdadbm@gmail.com>
2022-08-13 15:15:28 +0200

    Update ChocolatesByNumbers.java

--- codility/lessons/ChocolatesByNumbers.java
+++ codility/lessons/ChocolatesByNumbers.java
@@ -4,6 +4,7 @@ package com.codility.app.programmers.lessons.euclidean-algorithm.chocolates-by-n
 *	https://app.codility.com/ exercise solved by @author davidbalsasmartin
 **/
 
+// Solved using the Euclidean algorithm by division
 class Solution {
     public int solution(int N, int M) {
         return N / gcd(N, M);
@@ -11,6 +12,6 @@ class Solution {
     
     public int gcd(int a, int b) {
         int rest = a % b;
-	    return rest == 0 ? b : gcd(b, rest);
+	    return rest == 0 ? b : gcd(b, rest); // If the rest of the division is zero, return b, if not, call gcd with b and the rest of the division until find zero
 	}
 }
