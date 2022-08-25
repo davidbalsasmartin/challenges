a8420545a5ae3b6066858b29df7bad6bf66fe9ef
David Balsas Martín <dasdadbm@gmail.com>
2022-08-25 18:13:07 +0200

    Update NextGrowingNumber.java with javadoc

--- codingame/NextGrowingNumber.java
+++ codingame/NextGrowingNumber.java
@@ -13,6 +13,7 @@ class Solution {
 
         char[] charArray = n.toCharArray();
         String result = "";
+        // If the first char is 9, the next growing number will have all characteres, with one extra character = 1 (ex. 99 -> 111)
         if (charArray[0] == '9') {
             for (int i = 0; i <= charArray.length; i++) {
                 result += "1";
@@ -22,21 +23,21 @@ class Solution {
             char lastChar = 0;
 
             for (int i = 0; i < charArray.length; i++) {
-                if (changedNumber != null) {
+                if (changedNumber != null) { // When we find which is the char to grow, we only have to repeat it in the next iterations
                     result += changedNumber;
-                } else if (charArray[i] < lastChar) {
+                } else if (charArray[i] < lastChar) { // If we have a char that is not greater than the last one, take that 
                     result += lastChar;
                     changedNumber = Character.getNumericValue(lastChar);
-                } else if (charArray[i] == '9') {
+                } else if (charArray[i] == '9') { // If this char is 9, to be greatest, we have to increase the last number and copy it to this iteration
                     result = result.substring(0, result.length() - 1);
                     changedNumber = nextNumber(charArray[i - 1]);
                     result = result + changedNumber + changedNumber;
-                } else {
+                } else { // If this is a growing number at this moment, we can save it
                     result += charArray[i];
                     lastChar = charArray[i];
                 }
             }
-            if (changedNumber == null) {
+            if (changedNumber == null) { // If we do not have found the next growing number, take the last char and grow it
                 result = result.substring(0, result.length() - 1);
                 result += nextNumber(charArray[charArray.length - 1]);
             }
