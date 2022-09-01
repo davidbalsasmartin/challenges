04da30c9312307af1f90f077f30ab4d93789390b
David Balsas Martín <dasdadbm@gmail.com>
2022-09-01 20:57:24 +0200

    Create NotVerySecure.java

---
+++ codewars/NotVerySecure.java
@@ -0,0 +1,11 @@
+package com.codewars.counting-duplicates;
+
+/**
+ *	https://www.codewars.com/ challenge solved by @author davidbalsasmartin
+ **/
+ 
+ public class SecureTester{
+  public static boolean alphanumeric(String s){
+    return s.matches("^[a-zA-Z0-9]+$");
+  }
+}
