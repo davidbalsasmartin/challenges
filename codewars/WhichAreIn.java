9137ccd08fb30538a0f267b34a38607d198248ca
David Balsas Martín <dasdadbm@gmail.com>
2022-09-04 07:39:03 +0200

    Fixed indentation

--- codewars/WhichAreIn.java
+++ codewars/WhichAreIn.java
@@ -7,18 +7,17 @@ package com.codewars.which-are-in;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class WhichAreIn { 
-	
-	public static String[] inArray(String[] array1, String[] array2) {
-      Set<String> result = new TreeSet<>();
-      for (String string1 : array1) {
-        for (String string2 : array2) {
-          if (string2.contains(string1)) {
-            result.add(string1);
-            break;
-          }
+public class WhichAreIn { 	
+  public static String[] inArray(String[] array1, String[] array2) {
+    Set<String> result = new TreeSet<>();
+    for (String string1 : array1) {
+      for (String string2 : array2) {
+        if (string2.contains(string1)) {
+          result.add(string1);
+          break;
         }
       }
-		 return result.toArray(String[]::new);
-	}
+     }			
+     return result.toArray(String[]::new);
+  }
 }
