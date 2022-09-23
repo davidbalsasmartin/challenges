41c79a3b0c98c944959182a3274c0c7ca7fd44d5
David Balsas Martín <dasdadbm@gmail.com>
2022-09-23 13:30:40 +0200

    Create FindTheOddInt.java

---
+++ codewars/FindTheOddInt.java
@@ -0,0 +1,15 @@
+package com.codewars.find-the-odd-int;
+
+/**
+ *	https://www.codewars.com/ challenge solved by @author davidbalsasmartin
+ **/
+ 
+import java.util.Map;
+import java.util.stream.*;
+
+public class FindOdd {
+	public static int findIt(int[] a) {
+    return IntStream.of(a).boxed().collect(Collectors.groupingBy(i -> i, Collectors.counting())).entrySet()
+      .stream().filter(entry -> entry.getValue() % 2 != 0).map(entry -> entry.getKey()).findAny().get();
+  }
+}
