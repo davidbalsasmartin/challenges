0c6927aa601f57b6a1d7dbb28d65baf0f096377a
David Balsas Martín <dasdadbm@gmail.com>
2022-09-29 21:20:35 +0200

    Create MakeAFunctionThatDoesArithmetic.java

---
+++ codewars/MakeAFunctionThatDoesArithmetic.java
@@ -0,0 +1,21 @@
+package com.codewars.make-a-function-that-does-arithmetic;
+
+/**
+ *	https://www.codewars.com/ challenge solved by @author davidbalsasmartin
+ **/
+ 
+import java.util.function.BiFunction;
+import java.util.HashMap;
+
+class ArithmeticFunction {
+  private final static HashMap<String, BiFunction<Integer, Integer, Integer>> OPERATIONS = new HashMap<>() {{
+      put("add", (x, y) -> x + y);
+      put("subtract", (x, y) -> x - y);
+      put("multiply", (x, y) -> x * y);
+      put("divide", (x, y) -> x / y);
+    }};
+  
+  public static int arithmetic(int a, int b, String operator) {
+    return OPERATIONS.get(operator).apply(a, b);
+  }
+}
