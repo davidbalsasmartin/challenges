a4c32f00da80f3417ea003a0ff213e0e0341bfe7
David Balsas Martín <dasdadbm@gmail.com>
2022-10-02 15:26:52 +0200

    Create StringSplit.java

---
+++ codewars/StringSplit.java
@@ -0,0 +1,13 @@
+package com.codewars.string-split;
+
+/**
+ *	https://www.codewars.com/ challenge solved by @author davidbalsasmartin
+ **/
+
+public class StringSplit {
+    public static String[] solution(String s) {
+      if (s.length() % 2 != 0)
+        s += "_";
+        return s.split("(?<=\\G..)");
+    }
+}
