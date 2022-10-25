72bfa49901a05a8c53a27970fde0040b2d09e14d
David Balsas Martín <dasdadbm@gmail.com>
2022-10-25 20:10:36 +0200

    Deleted old imports and unused List

--- codewars/ArrayDiff.java
+++ codewars/ArrayDiff.java
@@ -5,12 +5,9 @@ package com.codewars.array-diff;
  **/
  
 import java.util.stream.IntStream;
-import java.util.List;
-import java.util.Arrays;
 
 public class Kata {
   public static int[] arrayDiff(int[] a, int[] b) {
-    List bList = Arrays.asList(b);
     return IntStream.of(a).filter(i -> !IntStream.of(b).anyMatch(x -> x == i)).toArray();
   }
 }
