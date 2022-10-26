7808081fe3415e31d95b748931dcac0d74eef01a
David Balsas Martín <dasdadbm@gmail.com>
2022-10-26 15:14:42 +0200

    Create AllUnique.java

---
+++ codewars/AllUnique.java
@@ -0,0 +1,12 @@
+package com.codewars.all-unique;
+
+/**
+ *	https://www.codewars.com/ challenge solved by @author davidbalsasmartin
+ **/
+
+
+public class Kata {
+    public static boolean hasUniqueChars(String str) {
+      return str.length() == str.chars().distinct().count();      
+    }
+}
