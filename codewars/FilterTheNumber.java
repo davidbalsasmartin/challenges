f002b6568f172e1eb71bc79115ff273f6ff20ca0
David Balsas Martín <dasdadbm@gmail.com>
2022-10-28 10:23:11 +0200

    Update FilterTheNumber.java - added Stream way to resolve it

--- codewars/FilterTheNumber.java
+++ codewars/FilterTheNumber.java
@@ -4,8 +4,18 @@ package com.codewars.filter-the-number;
  *	https://www.codewars.com/ challenge solved by @author davidbalsasmartin
  **/
 
+// ReplaceAll
 public class Kata {
     public static long filterString(final String value) {
         return Long.parseLong(value.replaceAll("[^0-9]+", ""));
+    }
+}
+
+// Streams
+import java.util.stream.Collectors;
+
+public class Kata {
+    public static long filterString(final String value) {
+        return Long.parseLong(value.chars().filter(c -> c > 47 && c < 58).mapToObj(i -> String.valueOf((char) i)).collect(Collectors.joining()));
     }
 }
