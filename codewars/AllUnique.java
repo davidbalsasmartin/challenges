bac1a39792a52df27afab2393f3d5b33b8cc8efe
David Balsas Martín <dasdadbm@gmail.com>
2022-10-30 16:06:26 +0100

    Update AllUnique.java - formatted

--- codewars/AllUnique.java
+++ codewars/AllUnique.java
@@ -4,7 +4,6 @@ package com.codewars.all-unique;
  *	https://www.codewars.com/ challenge solved by @author davidbalsasmartin
  **/
 
-
 public class Kata {
     public static boolean hasUniqueChars(String str) {
       return str.length() == str.chars().distinct().count();      
