0dd2f72672bee6be176fe73d9f566b71b0d38c2e
David Balsas Martín <dasdadbm@gmail.com>
2022-10-30 15:33:23 +0100

    Update SpeedLimit.java - added IntStream way

--- codewars/SpeedLimit.java
+++ codewars/SpeedLimit.java
@@ -20,3 +20,27 @@ public class Kata {
       return result;
     }
 }
+
+
+// Same with IntStream
+import java.util.stream.IntStream;
+
+public class Kata {
+  
+    public static int speedLimit(final int speed, final int[] signals) {
+      return IntStream.of(signals).map(signal -> fine(speed, signal)).sum();
+    }
+  
+    private static int fine(int speed, int maxSpeed) {
+      int difference = speed - maxSpeed;
+      int fine = 0;
+      if (difference >= 10 && difference <= 19) {
+        fine = 100;
+      } else if (difference >= 20 && difference <= 29) {
+        fine = 250;
+      } else if (difference >= 30) {
+        fine = 500;
+      }
+      return fine;
+    }
+}
