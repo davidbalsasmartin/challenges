76db297ed53322b9f1c7d4ae8f73005489100254
David Balsas Martín <dasdadbm@gmail.com>
2022-11-24 17:00:05 +0100

    Update NumberedCards.java

--- edabit/NumberedCards.java
+++ edabit/NumberedCards.java
@@ -1,11 +1,19 @@
+package com.edabit.challenge.numbered-cards;
+
+/**
+ *	https://edabit.com/ challenge solved by @author davidbalsasmartin
+ **/
+
+import java.util.Arrays;
+
 public class NumberedCard {
   public static boolean winRound(int[] you, int[] opp) {
-		return getMaxComb(you) > getMaxComb(opp);
-	}
+    return getMaxComb(you) > getMaxComb(opp);
+  }
 	
-	private static int getMaxComb(int[] arr) {
-		Arrays.sort(arr);
-		// The info says that there are five number always
-		return Integer.parseInt("" + arr[4] + arr[3]);
-	}
+  private static int getMaxComb(int[] arr) {
+    Arrays.sort(arr);
+    // The info says that there are five number always
+    return Integer.parseInt("" + arr[4] + arr[3]);
+  }
 }
