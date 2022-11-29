f7a5b0f1f8c7b9ae037b2518ab62a22e955731e9
David Balsas Martín <dasdadbm@gmail.com>
2022-11-29 19:56:03 +0100

    Create ReverseOddWords.java

---
+++ edabit/ReverseOddWords.java
@@ -0,0 +1,21 @@
+package com.edabit.challenge.ReverseOddWords;
+
+/**
+ *	https://edabit.com/ challenge solved by @author davidbalsasmartin
+ **/
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Challenge {
+	public static String reverseOdd(String str) {
+		return Arrays.stream(str.split(" ")).map(i -> i.length() % 2 == 0 ? i : reverse(i)).collect(Collectors.joining(" "));
+	}
+	
+	private static String reverse(String str) {
+		StringBuilder strB = new StringBuilder();
+		strB.append(str);
+		strB.reverse();
+		return strB.toString();
+	}
+}
