29176a502c11eef7d511b59529b6a5b59b4aec2d
David Balsas Martín <dasdadbm@gmail.com>
2022-12-01 13:15:11 +0100

    Redone after finding a possible error

--- edabit/FindTheDifference.java
+++ edabit/FindTheDifference.java
@@ -4,14 +4,17 @@ package com.edabit.challenge.find-the-difference;
  *	https://edabit.com/ challenge solved by @author davidbalsasmartin
  **/
  
- import java.util.List;
- import java.util.stream.Collectors;
- 
  public class LetterDifference {
 	public static char findTheDifference(String s, String t) {
-		List<Character> charsS = s.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
-		List<Character> charsT = t.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
-		charsT.removeAll(charsS);
-		return charsT.get(0);
+		char result = '0';
+		// Sum one space to avoid StringIndexOutOfBoundsException
+		s += " ";
+		for (int i = 0; i < t.length(); i++) {
+			if (t.charAt(i) != s.charAt(i)) {
+				result = t.charAt(i);
+				break;
+			}
+		}
+		return result;
 	}
 }
