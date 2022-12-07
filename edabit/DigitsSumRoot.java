d913504588c571866543d99e4d511ee2300b1a8f
David Balsas Martín <dasdadbm@gmail.com>
2022-12-07 14:27:27 +0100

    Done DigitsSumRoot with recursion too

--- edabit/DigitsSumRoot.java
+++ edabit/DigitsSumRoot.java
@@ -7,14 +7,26 @@ package com.edabit.challenge.digits-sum-roots;
 public class Root {
 	public static int digitRoot(long n) {
 		int sum = 0;
-		do {
+		while (n > 10) {
 			do {
 				sum += n % 10;
 				n /= 10;
 			} while (n != 0);
 			n = sum;
 			sum = 0;
-		} while(n > 10);
+		}
 		return (int) n;
+	}
+}
+
+// Done it with recursion
+public class Root {
+	public static int digitRoot(long n) {
+		int sum = 0;
+		while (n != 0) {
+			sum += n % 10;
+			n /= 10;
+		}
+		return sum < 10 ? (int) sum : digitRoot(sum);
 	}
 }
