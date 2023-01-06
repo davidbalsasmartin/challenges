8a12d945894a5a55f9dbc9c90c11be9835899c58
David Balsas Martín <dasdadbm@gmail.com>
2023-01-06 10:47:06 +0100

    Create RecursiveDigit.java

---
+++ edabit/RecursiveDigit.java
@@ -0,0 +1,22 @@
+package com.edabit.challenge.recursive-digit;
+
+/**
+ *	https://edabit.com/ challenge solved by @author davidbalsasmartin
+ **/
+ 
+ // Pending of revision, it is not finished
+ 
+public class Challenge {
+  public static int superDigit(String n, int k) {
+    int sum = 0;
+    for (int i = 0; i <= k && n.length() > 1; i++) {
+      sum = 0;
+      for (int counter = 0; counter < n.length(); counter++) {
+        sum += Character.getNumericValue(n.charAt(counter));
+      }
+      n = Integer.toString(sum);
+      System.out.println("vuelta " + i + " n = " + n);
+    }
+    return sum;
+  }
+}
