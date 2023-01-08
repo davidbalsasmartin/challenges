c0c52c13907c02c5ce88bd7876810f82244fddf4
David Balsas Martín <dasdadbm@gmail.com>
2023-01-08 18:09:18 +0100

    Create SpartansCipher.java

---
+++ edabit/SpartansCipher.java
@@ -0,0 +1,23 @@
+package com.edabit.challenge.spartans-cipher;
+
+/**
+ *	https://edabit.com/ challenge solved by @author davidbalsasmartin
+ **/
+
+// It is not finished, it returns a false response when we have to add spaces.
+
+public class GreekSpartans {
+  public static String cipher(String message, int nSlide) {
+      StringBuilder result = new StringBuilder();
+      if (message.length() % nSlide != 0)
+          message += " ".repeat((message.length() / nSlide) + 1);
+      int counter = 0;
+      for (int i = 0; i < message.length(); i++) {
+        result.append(message.charAt(counter));
+        counter += message.length() / nSlide;
+        if (counter >= message.length())
+          counter = counter + 1 - message.length();
+      }
+      return result.toString().trim();
+    }
+  }
