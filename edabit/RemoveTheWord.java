e0c5422b533a1d96b67a522a80618bc58f53b102
David Balsas Martín <dasdadbm@gmail.com>
2023-01-12 15:38:50 +0100

    RemoveTheWord.java done it in a better way

--- edabit/RemoveTheWord.java
+++ edabit/RemoveTheWord.java
@@ -4,7 +4,7 @@ package com.edabit.challenge.remove-the-word;
  *	https://edabit.com/ challenge solved by @author davidbalsasmartin
  **/
  
- public class Challenge {
+public class Challenge {
 	public static String[] removeLetters(String[] letters, String word) {
 		List<String> result = new ArrayList<>();
 		List<String> wordList = new ArrayList<>(Arrays.asList(word.split("")));
@@ -14,3 +14,13 @@ package com.edabit.challenge.remove-the-word;
 		return result.stream().toArray(String[]::new);
 	}
 }
+
+// Better way to solve it
+public class Challenge {
+	public static String[] removeLetters(String[] letters, String word) {
+		List<String> wordList = new ArrayList<>(Arrays.asList(letters));
+		for (String letter : word.split(""))
+			wordList.remove(letter);
+		return wordList.stream().toArray(String[]::new);
+	}
+ }
