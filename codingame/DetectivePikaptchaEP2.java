a02d8991bc069844ff4d5cb1210a87c820a4f136
David Balsas Martín <dasdadbm@gmail.com>
2023-02-15 17:30:33 +0100

    Update DetectivePikaptchaEP2.java

--- codingame/DetectivePikaptchaEP2.java
+++ codingame/DetectivePikaptchaEP2.java
@@ -4,7 +4,6 @@ package com.codingame.ide.puzzle.detective-pikaptcha-ep2;
  *	https://www.codingame.com/ puzzle created by user java_coffee_cup and solved by @author davidbalsasmartin
  **/
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
@@ -14,43 +13,31 @@ class Player {
     private static char sign = '>';
     private static int x = 0;
     private static int y = 0;
+    private static StringBuilder[] map;
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int width = in.nextInt();
-        int height = in.nextInt();
-        StringBuilder[] map = new StringBuilder[height];
-        Pattern pattern = Pattern.compile("[<>v^]");
-        for (int i = 0; i < height; i++) {
-            map[i] = new StringBuilder(in.next());
-            Matcher matcher = pattern.matcher(map[i]);
-            if (matcher.find()) {
-                x = matcher.start();
-                y = i;
-                sign = map[y].charAt(x);
-            }
-        }
+        final int width = in.nextInt();
+        final int height = in.nextInt();
+        map = new StringBuilder[height];
+        getMapAndXYInfo(in, map);
+        
         final int firstX = x;
         final int firstY = y;
         char side = in.next().charAt(0);
         
         do {
-            int[] directions = changeDirection(side);
-            int currentX = directions[0];
-            int currentY = directions[1];
-            if (currentX >= 0 && currentX < width && currentY >= 0 && currentY < height && map[currentY].charAt(currentX) != '#') {
-                x = currentX;
-                y = currentY;
-                map[y].setCharAt(x, (char) (Character.isDigit(map[y].charAt(x)) ? map[y].charAt(x) + 1 : 49));
-            } else {
+            int[] axis = changeDirection(side);
+            if (!checkAndSetAxisAndMap(axis, width, height)) {
+                int count = 0;
+                boolean goodDirection = false;
                 do {
-                    directions = changeDirection(side == 'L' ? 'R' : 'L');
-                    currentX = directions[0];
-                    currentY = directions[1];
-                } while (!(currentX >= 0 && currentX < width && currentY >= 0 && currentY < height && map[currentY].charAt(currentX) != '#'));
-                x = currentX;
-                y = currentY;
-                map[y].setCharAt(x, (char) (Character.isDigit(map[y].charAt(x)) ? map[y].charAt(x) + 1 : 49));
+                    count++;
+                    axis = changeDirection(side == 'L' ? 'R' : 'L');
+                    goodDirection = checkAndSetAxisAndMap(axis, width, height);
+                } while (count < 3 && !goodDirection);
+                if (!goodDirection) // Set 0 and return, it is trapped
+                   map[y].setCharAt(x, (char) 48);
             }
         } while(x != firstX || y != firstY);
         print(map);
@@ -88,6 +75,32 @@ class Player {
             }
         }
         return directions;
+    }
+
+    private static boolean checkAndSetAxisAndMap(int[] axis, int width, int height) {
+        boolean result = false;
+        int currentX = axis[0];
+        int currentY = axis[1];
+        if (currentX >= 0 && currentX < width && currentY >= 0 && currentY < height && map[currentY].charAt(currentX) != '#') {
+            result = true;
+            x = axis[0];
+            y = axis[1];
+            map[y].setCharAt(x, (char) (Character.isDigit(map[y].charAt(x)) ? map[y].charAt(x) + 1 : 49));
+        }
+        return result;
+    }
+
+    private static void getMapAndXYInfo(Scanner in, StringBuilder[] map) {
+        Pattern pattern = Pattern.compile("[<>v^]");
+        for (int i = 0; i < map.length; i++) {
+            map[i] = new StringBuilder(in.next());
+            Matcher matcher = pattern.matcher(map[i]);
+            if (matcher.find()) {
+                x = matcher.start();
+                y = i;
+                sign = map[y].charAt(x);
+            }
+        }
     }
 
     private static void print(StringBuilder[] map) {
