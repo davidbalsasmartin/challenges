8b64c053c87415d9eb7972c715b255d6526d7083
David Balsas Martín <dasdadbm@gmail.com>
2023-02-19 21:16:45 +0100

    Update CapitalSplit.java

--- edabit/CapitalSplit.java
+++ edabit/CapitalSplit.java
@@ -5,7 +5,7 @@ package com.edabit.challenge.capital-split;
  **/
 
 public class Challenge {
-  public static String capSpace(String txt) {
+	public static String capSpace(String txt) {
 		StringBuilder result = new StringBuilder();
 		for (char c : txt.toCharArray()) {
 			if (Character.isUpperCase(c)) {
@@ -15,5 +15,15 @@ public class Challenge {
 			}
 		}
 		return result.toString();
+	}
+}
+
+// Done it with Java 8 Stream
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.joining;
+
+public class Challenge {
+	public static String capSpace(String txt) {
+		return Stream.of(txt.split("")).map(i -> Character.isUpperCase(i.charAt(0)) ? (" " + Character.toLowerCase(i.charAt(0))) : i).collect(joining(""));
 	}
 }
